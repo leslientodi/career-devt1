2714dc65ca0b93c751088657e7116991e16dc4ac
leslientodi <lesliehazard500@gmail.com>
2023-12-07 12:26:32 +0000

    correct general arts

--- careerdevt.js
+++ careerdevt.js
@@ -28,64 +28,66 @@ function selectProgram() {
     document.querySelector('.program-info').innerHTML = 
     `<p>You are offering ${program}<p> <h2>Now Enter Your Grades</h2> 
     <h3>Elective Subjects</h3>
-    <p><label>Government</label><input type='number' min='1' max='9'></p>
-    <p><label>History</label><input type='number' min='1' max='9'></p>
-    <p><label>E-Maths</label><input type='number' min='1' max='9'></p>
-    <p><label>Economics</label><input type='number' min='1' max='9'></p>
+    <p><label>Government</label><input class='government' type='number' min='1' max='9'></p>
+    <p><label>History</label><input class='history' type='number' min='1' max='9'></p>
+    <p><label>E-Maths</label><input class='emaths' type='number' min='1' max='9'></p>
+    <p><label>Economics</label><input class='economics' type='number' min='1' max='9'></p>
     
     <h3>Core Subjects</h3>
-    <p><label>C-Maths</label><input type='number' min='1' max='9'></p>
-    <p><label>English</label><input type='number' min='1' max='9'></p>
-    <p><label>Int-Science</label><input type='number' min='1' max='9'></p>
-    <p><label>Social Studies</label><input type='number' min='1' max='9'></p>
-    <button>Submit</button>`;
+    <p><label>C-Maths</label><input class='cmaths' type='number' min='1' max='9'></p>
+    <p><label>English</label><input class='english' type='number' min='1' max='9'></p>
+    <p><label>Int-Science</label><input class='intScience' type='number' min='1' max='9'></p>
+    <p><label>Social Studies</label><input class='social' type='number' min='1' max='9'></p>
+    <button onclick="careerGeneralArts();">Submit</button>`;
     }
     else if (program === 'BUSINESS') {
 
     document.querySelector('.program-info').innerHTML = 
     `<p>You are offering ${program}<p> <h2>Now Enter Your Grades</h2> 
     <h3>Elective Subjects</h3>
-    <p><label>Financial Accounting</label><input type='number' min='1' max='9'></p>
-    <p><label>Business Management</label><input type='number' min='1' max='9'></p>
-    <p><label>E-Maths</label><input type='number' min='1' max='9'></p>
-    <p><label>Economics</label><input type='number' min='1' max='9'></p>
-    <p><label>Cost Accounting</label><input type='number' min='1' max='9'></p>
+    <p><label>Financial Accounting</label><input class='fa' type='number' min='1' max='9'></p>
+    <p><label>Business Management</label><input class='bm' type='number' min='1' max='9'></p>
+    <p><label>E-Maths</label><input class='emaths' type='number' min='1' max='9'></p>
+    <p><label>Economics</label><input class='economics' type='number' min='1' max='9'></p>
+    <p><label>Cost Accounting</label><input class='costing' type='number' min='1' max='9'></p>
   
     <h3>Core Subjects</h3>
-    <p><label>C-Maths</label><input type='number' min='1' max='9'></p>
-    <p><label>English</label><input type='number' min='1' max='9'></p>
-    <p><label>Int-Science</label><input type='number' min='1' max='9'></p>
-    <p><label>Social Studies</label><input type='number' min='1' max='9'></p>
-    <button>Submit</button>`;
+    <p><label>C-Maths</label><input class='cmaths' type='number' min='1' max='9'></p>
+    <p><label>English</label><input class='english' type='number' min='1' max='9'></p>
+    <p><label>Int-Science</label><input class='intScience' type='number' min='1' max='9'></p>
+    <p><label>Social Studies</label><input class='social' type='number' min='1' max='9'></p>
+    <button onclick="careerBusiness();">Submit</button>`;
     }
     else if (program === 'HOME ECONOMICS') {
 
     document.querySelector('.program-info').innerHTML = 
     `<p>You are offering ${program}<p> <h2>Now Enter Your Grades</h2> 
     <h3>Elective Subjects</h3>
-    <p><label>Catering and Sewing</label><input type='number' min='1' max='9'></p>
-    <p><label>Food and Nutrition</label><input type='number' min='1' max='9'></p>
-    <p><label>Management in Living</label><input type='number' min='1' max='9'></p>
+    <p><label>Catering and Sewing</label><input class='cas' type='number' min='1' max='9'></p>
+    <p><label>Food and Nutrition</label><input class='fan' type='number' min='1' max='9'></p>
+    <p><label>Management in Living</label><input class='mil' type='number' min='1' max='9'></p>
+    <p><label>Biology</label><input class='biology' type='number' min='1' max='9'></p>
+    <p><label>Chemistry</label><input class='chemistry' type='number' min='1' max='9'></p>
     
     <h3>Core Subjects</h3>
-    <p><label>C-Maths</label><input type='number' min='1' max='9'></p>
-    <p><label>English</label><input type='number' min='1' max='9'></p>
-    <p><label>Int-Science</label><input type='number' min='1' max='9'></p>
-    <p><label>Social Studies</label><input type='number' min='1' max='9'></p>
-    <button>Submit</button>`;
+    <p><label>C-Maths</label><input class='cmaths' type='number' min='1' max='9'></p>
+    <p><label>English</label><input class='english' type='number' min='1' max='9'></p>
+    <p><label>Int-Science</label><input class='intScience' type='number' min='1' max='9'></p>
+    <p><label>Social Studies</label><input class='social' type='number' min='1' max='9'></p>
+    <button onclick="careerHomeEcons();">Submit</button>`;
     }
     else if (program === 'VISUAL ARTS') {
 
     document.querySelector('.program-info').innerHTML = 
     `<p>You are offering ${program}<p> <h2>Now Enter Your Grades</h2> 
     <h3>Elective Subjects</h3>
-    <p><label>Painting</label><input type='number' min='1' max='9'></p>
-    <p><label>Drawing</label><input type='number' min='1' max='9'></p>
-    <p><label>Printmaking</label><input type='number' min='1' max='9'></p>
-    <p><label>Sculpture</label><input type='number' min='1' max='9'></p>
-    <p><label>Photography</label><input type='number' min='1' max='9'></p>
-    <p><label>Design</label><input type='number' min='1' max='9'></p>
-    <p><label>Crafts</label><input type='number' min='1' max='9'></p>
+    <p><label>General knowledge in Art</label><input class='gka' type='number' min='1' max='9'></p>
+    <p><label>Textiles</label><input class='textiles' type='number' min='1' max='9'></p>
+    <p><label>Picture making</label><input class='picture' type='number' min='1' max='9'></p>
+    <p><label>Ceramics & Sculpture</label><input class='ceramics' type='number' min='1' max='9'></p>
+    <p><label>Basketry</label><input class='basketry' type='number' min='1' max='9'></p>
+    <p><label>Graphic Design</label><input class='graphic' type='number' min='1' max='9'></p>
+    <p><label>Leather work</label><input class='leather' type='number' min='1' max='9'></p>
     <h3>Core Subjects</h3>
     <p><label>C-Maths</label><input type='number' min='1' max='9'></p>
     <p><label>English</label><input type='number' min='1' max='9'></p>
@@ -650,6 +652,286 @@ function careerScience () {
 };
 
 function careerGeneralArts (){
+    const gkaInput = document.querySelector('.gka');
+    gkaGrade = gkaInput.value;
     
+    const textilesInput = document.querySelector('.textiles');
+    textilesGrade = textilesInput.value;
+    const pictureInput = document.querySelector('.picture');
+    pictureGrade = pictureInput.value;
+    const ceramicsInput = document.querySelector('.ceramics');
+    ceramicsGrade = ceramicsInput.value;
+    const graphicInput = document.querySelector('.graphic');
+    graphicGrade = graphicInput.value;
+    const basketryInput = document.querySelector('.basketry');
+    basketryGrade = basketryInput.value;
+    const leatherInput = document.querySelector('.leather');
+    leatherGrade = leatherInput.value;
+
+   
+    const cmathsInput = document.querySelector('.cmaths');
+    cmathsGrade = cmathsInput.value;
+    const englishInput = document.querySelector('.english');
+    englishGrade = englishInput.value;
+    const intScienceInput = document.querySelector('.intScience');
+    intScienceGrade = intScienceInput.value;
+    const socialInput = document.querySelector('.social');
+    socialGrade = socialInput.value;
+    
+    if (governmentGrade === '1' && englishGrade=== '1' && socialGrade === '1' && historyGrade < 3 ){
+        document.querySelector('.entire').innerHTML=`
+        <h2>LLB LAW</h2>`
+    }
+
+    else if (cmathsGrade < 3 && economicsGrade < 3){
+        document.querySelector('.entire').innerHTML=`
+        <h2>Bsc Economics</h2>`
+    }
+    else {
+        document.querySelector('.entire').innerHTML=`
+        No Career matches your info`
+    }
+
+};
+
+function careerBusiness (){
+    const faInput = document.querySelector('.fa');
+    faGrade = faInput.value;
+    
+    const bmInput = document.querySelector('.bm');
+    bmGrade = bmInput.value;
+    const emathsInput = document.querySelector('.emaths');
+    emathsGrade = emathsInput.value;
+    const economicsInput = document.querySelector('.economics');
+    economicsGrade = economicsInput.value;
+    const costingInput = document.querySelector('.costing');
+    costingGrade = costingInput.value;
+    
+   
+    const cmathsInput = document.querySelector('.cmaths');
+    cmathsGrade = cmathsInput.value;
+    const englishInput = document.querySelector('.english');
+    englishGrade = englishInput.value;
+    const intScienceInput = document.querySelector('.intScience');
+    intScienceGrade = intScienceInput.value;
+    const socialInput = document.querySelector('.social');
+    socialGrade = socialInput.value;
+
+    if (bmGrade === '1' && emathsGrade < 3 && cmathsGrade < 3 && faGrade < 3 && costingGrade < 3){
+        document.querySelector('.entire').innerHTML=`
+        <h2>Business Administration(Banking/Accounting/Finance)</h2>`
+
+    }
+    else if (economicsGrade === '1' ){
+        document.querySelector('.entire').innerHTML=`
+         <h2>Economics</h2>`
+
+    }
+    else if (englishGrade === "1" && socialGrade === "1"){
+        document.querySelector('.entire').innerHTML = `
+        <h2>Select your field of interest</h2>
+        <select class="fields-HM">
+           
+           <option>The Constitution</option>
+           <option>Politics</option>
+           <option>Societal norms</option>
+           <option>Human affairs</option>
+           
+
+           
+        
+        
+        
+        
+        </select>
+        <button class="submit"
+        
+        >Submit</button>`
+
+        const btn = document.querySelector('.submit');
+        const sb = document.querySelector('.fields-HM');
+
+        btn.onclick = (event) => {
+            if (sb.selectedIndex === 0){
+                document.querySelector('.entire').innerHTML=`
+                <h2>Llb Law</h2>
+                `;
+            }
+            else if (sb.selectedIndex === 1){
+                document.querySelector('.entire').innerHTML=`
+                <h2>Bsc Political Science</h2>`;
+            }
+            else if (sb.selectedIndex === 2){
+                document.querySelector('.entire').innerHTML=`
+                <h2>Bsc Sociology</h2>`;
+            }
+            else if (sb.selectedIndex === 3){
+                document.querySelector('.entire').innerHTML=`
+                <h2>BA Journalism</h2>`;
+            }
+            
+            else {
+                console.log('invalid')
+            }
+        }
+    }
+    else {
+        document.querySelector('.entire').innerHTML=`
+        No Career matches your info`
+    }
+};
+
+function careerHomeEcons(){
+    const casInput = document.querySelector('.cas');
+    casGrade = casInput.value;
+    const fanInput = document.querySelector('.fan');
+    fanGrade = fanInput.value;
+    const milInput = document.querySelector('.mil');
+    milGrade = milInput.value;
+    const biologyInput = document.querySelector('.biology');
+    biologyGrade = biologyInput.value;
+    const chemistryInput = document.querySelector('.chemistry');
+    chemistryGrade = chemistryInput.value;
+    
+    
+   
+    const cmathsInput = document.querySelector('.cmaths');
+    cmathsGrade = cmathsInput.value;
+    const englishInput = document.querySelector('.english');
+    englishGrade = englishInput.value;
+    const intScienceInput = document.querySelector('.intScience');
+    intScienceGrade = intScienceInput.value;
+    const socialInput = document.querySelector('.social');
+    socialGrade = socialInput.value;
+
+    if (casGrade === '1' && milGrade < 3){
+        document.querySelector('.entire').innerHTML=`
+         <h2>Fashion Design</h2>`
+    }
+    else if (fanGrade === '1' && chemistryGrade < 4 && chemistryGrade > 0){
+        document.querySelector('.entire').innerHTML=`
+         <h2>Bsc Dietetics</h2>
+         <h2>Bsc food science</h2>`
+    }
+    else if (biologyGrade === '1' && intScienceGrade < 4 && cmathsGrade < 4){
+        document.querySelector('.entire').innerHTML = `
+        <h2>Select your field of interest</h2>
+        <select class="fields-HM">
+           
+           <option>Physiology</option>
+           <option>Reproduction</option>
+           
+
+           
+        
+        
+        
+        
+        </select>
+        <button class="submit"
+        
+        >Submit</button>`
+
+        const btn = document.querySelector('.submit');
+        const sb = document.querySelector('.fields-HM');
+
+        btn.onclick = (event) => {
+            if (sb.selectedIndex === 0){
+                document.querySelector('.entire').innerHTML=`
+                <h2>Bsc Nursing</h2>`;
+            }
+            else if (sb.selectedIndex === 1){
+                document.querySelector('.entire').innerHTML=`
+                <h2>Bsc Midwifery</h2>
+                `;
+            }
+       
+        
+            else {
+                console.log('invalid')
+            }
+        }
+
+    }
+    else if (englishGrade === "1" && socialGrade === "1"){
+        document.querySelector('.entire').innerHTML = `
+        <h2>Select your field of interest</h2>
+        <select class="fields-HM">
+           
+           <option>The Constitution</option>
+           <option>Politics</option>
+           <option>Societal norms</option>
+           <option>Human affairs</option>
+           
+
+           
+        
+        
+        
+        
+        </select>
+        <button class="submit"
+        
+        >Submit</button>`
+
+        const btn = document.querySelector('.submit');
+        const sb = document.querySelector('.fields-HM');
+
+        btn.onclick = (event) => {
+            if (sb.selectedIndex === 0){
+                document.querySelector('.entire').innerHTML=`
+                <h2>Llb Law</h2>
+                `;
+            }
+            else if (sb.selectedIndex === 1){
+                document.querySelector('.entire').innerHTML=`
+                <h2>Bsc Political Science</h2>`;
+            }
+            else if (sb.selectedIndex === 2){
+                document.querySelector('.entire').innerHTML=`
+                <h2>Bsc Sociology</h2>`;
+            }
+            else if (sb.selectedIndex === 3){
+                document.querySelector('.entire').innerHTML=`
+                <h2>BA Journalism</h2>`;
+            }
+            
+            else {
+                console.log('invalid')
+            }
+        }
+    }
+
+}
+
+
+function careerVisualArts (){
+    const gkaInput = document.querySelector('.gka');
+    gkaGrade = gkaInput.value;
+    
+    const textilesInput = document.querySelector('.textiles');
+    textilesGrade = textilesInput.value;
+    const pictureInput = document.querySelector('.picture');
+    pictureGrade = pictureInput.value;
+    const ceramicsInput = document.querySelector('.ceramics');
+    ceramicsGrade = ceramicsInput.value;
+    const graphicInput = document.querySelector('.graphic');
+    graphicGrade = graphicInput.value;
+    const basketryInput = document.querySelector('.basketry');
+    basketryGrade = basketryInput.value;
+    const leatherInput = document.querySelector('.leather');
+    leatherGrade = leatherInput.value;
+    
+    
+   
+    const cmathsInput = document.querySelector('.cmaths');
+    cmathsGrade = cmathsInput.value;
+    const englishInput = document.querySelector('.english');
+    englishGrade = englishInput.value;
+    const intScienceInput = document.querySelector('.intScience');
+    intScienceGrade = intScienceInput.value;
+    const socialInput = document.querySelector('.social');
+    socialGrade = socialInput.value;
+
 }
 
